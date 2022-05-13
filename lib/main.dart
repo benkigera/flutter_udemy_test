f70e3b4e2224369150f5fd3fc0ecd612e905a512
Benson Kigera <benkigera@gmail.com>
2022-05-13 14:41:05 +0300

    Change widget to Row

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import './answer.dart';
 import './question.dart';
+import './skip.dart';
 
 // method with only one expression
 void main() => runApp(MyApp());
@@ -15,6 +16,20 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
+  final questions = const [
+    {
+      'questionText': "What's your fav color",
+      "answers": ['Black', 'Red', 'Green', 'White']
+    },
+    {
+      'questionText': "What's your fav animal",
+      "answers": ['elephant', 'Lion', 'Rabbit', 'Snake']
+    },
+    {
+      'questionText': "What's your fav meal",
+      "answers": ['Pizza', 'Steak', 'Ugali', 'Chicken wings']
+    },
+  ];
 
   void _answerQuestions() {
     setState(() {
@@ -32,7 +47,7 @@ class _MyAppState extends State<MyApp> {
         body: _questionIndex < questions.length
             ? Column(
                 children: [
-                  Skip("hello", Icons.visibility_off),
+                  Skip("Welcome...", Icons.visibility_off),
                   Questions(questions[_questionIndex]['questionText']),
                   ...(questions[_questionIndex]['answers'] as List<String>)
                       .map((answer) {
--- lib/skip.dart
+++ lib/skip.dart
@@ -4,18 +4,26 @@ class Skip extends StatelessWidget {
   final String clicked;
   final IconData icons;
 
-  Skip(this.clicked,this.icons);
+  Skip(this.clicked, this.icons);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       margin: EdgeInsets.all(10),
-      child: Text(clicked,
-        textAlign: TextAlign.center,
-        style: TextStyle(
-        fontSize: 26
-      ),
+      child: Row(
+        //row contents horizontally
+        crossAxisAlignment: CrossAxisAlignment.center,
+        //row contents vertically
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            clicked,
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 26),
+          ),
+          Icon(Icons.visibility_off)
+        ],
       ),
     );
   }
